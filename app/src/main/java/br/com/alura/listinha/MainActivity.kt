b21dea7c49f17e2bb5a9b18122626875db1f6088
Adler Santos <adlercoelhosantos12@gmail.com>
2024-02-23 11:40:49 -0300

    feat:(layout) implemetando arquivo xml de layout

--- app/src/main/java/br/com/alura/listinha/MainActivity.kt
+++ app/src/main/java/br/com/alura/listinha/MainActivity.kt
@@ -2,14 +2,14 @@ package br.com.alura.listinha
 
 import android.app.Activity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.widget.Toast
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Mariana te amo <3", Toast.LENGTH_LONG).show()
+        // Toast.makeText(this, "Mariana te amo <3", Toast.LENGTH_LONG).show()
+        setContentView(R.layout.activity_main)
     }
 
 }
