d19a9658c2e6d03d1be73914d83d086ede120765
Adler Santos <adlercoelhosantos12@gmail.com>
2024-02-26 17:33:15 -0300

    feat:(layout) adicionando o recycler view

--- app/src/main/java/br/com/alura/listinha/MainActivity.kt
+++ app/src/main/java/br/com/alura/listinha/MainActivity.kt
@@ -10,6 +10,14 @@ class MainActivity : Activity() {
         super.onCreate(savedInstanceState)
         // Toast.makeText(this, "Mariana te amo <3", Toast.LENGTH_LONG).show()
         setContentView(R.layout.activity_main)
+
+        val inputName = findViewById<TextView>(R.id.name);
+        val inputDesc = findViewById<TextView>(R.id.description);
+        val inputValue = findViewById<TextView>(R.id.value);
+
+        inputName.text = "Cesta de Frutas"
+        inputDesc.text = "Uva e Banana"
+        inputValue.text = "R$ 19,99"
     }
 
 }
